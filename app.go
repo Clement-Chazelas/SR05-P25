bf4b06f38e75e429a9a5a21f995e5a1f6dad2bc9
Kinshoop <82682248+Kinshoop@users.noreply.github.com>
2025-05-13 18:51:16 +0200

    Ajout couleur print app.go

--- app.go
+++ app.go
@@ -13,6 +13,14 @@ import (
 	"time"
 )
 
+var rouge string = "\033[1;31m"
+var orange string = "\033[1;33m"
+var vert string = "\033[1;32m"
+var cyan string = "\033[1;36m"  // Cyan (Gras)
+var blanc string = "\033[1;37m" // Blanc/Gris clair (Gras)
+
+var raz string = "\033[0;00m"
+
 var blockChain Blockchain
 
 var adressOfSite []ecdsa.PublicKey
@@ -26,7 +34,7 @@ var allowSC = false
 
 var Nom string
 
-var l = log.New(os.Stderr, "", 0)
+var stderr = log.New(os.Stderr, "", 0)
 
 func sendInitialisation() {
 	isKeySent := false
@@ -35,7 +43,7 @@ func sendInitialisation() {
 			//J'envoie ma clé aux autres sites (une seule fois)
 			mutex.Lock()
 
-			l.Println(Nom, sitePubKey)
+			stderr.Println(Nom, "J'envoie ma clé aux autres sites")
 			fmt.Printf("K:%s\n", SendPublicKey(&sitePubKey))
 
 			mutex.Unlock()
@@ -45,19 +53,19 @@ func sendInitialisation() {
 
 		if len(adressOfSite) >= nbSite-1 && sitePubKey.X.Cmp(adressOfSite[0].X) == 1 && sitePubKey.X.Cmp(adressOfSite[1].X) == 1 {
 			//j'ai reçu tt les clés + j'ai la clé la plus grande
-			l.Println(Nom, "Je suis l'initialisateur")
+			stderr.Println(blanc, Nom, "Je suis l'initialisateur de la blockchain", raz)
 			mutex.Lock()
 
 			blockChain.InitBlockchain(append(adressOfSite, sitePubKey))
 			fmt.Printf("B:%s\n", SendBlock(blockChain.GetLastBlock()))
 
 			mutex.Unlock()
-			l.Println(Nom, "Initialisation terminée")
+			stderr.Println(blanc, Nom, "Initialisation terminée", raz)
 			go sendMain()
 			break
 		} else if len(blockChain.Chain) > 0 {
 			// J'attends le premier bloc de la chaine
-			l.Println(Nom, "Initialisation terminée")
+			stderr.Println(cyan, Nom, "Initialisation terminée", raz)
 			go sendMain()
 			break
 		}
@@ -74,7 +82,7 @@ func sendMain() {
 			// Verif si autorisation d'accès à la section critique
 			if allowSC {
 				mutex.Lock()
-				l.Println(Nom, "Initialisation d'un nouveau bloc")
+				stderr.Println(Nom, "Initialisation d'un nouveau bloc")
 
 				newBlock := InitBlock(pendingTransactions, blockChain.GetLastBlock().Hash, blockChain.GetLastBlock().UTXOs)
 				newBlock.MineBlock()
@@ -84,7 +92,7 @@ func sendMain() {
 				allowSC = false
 				isSCAsked = false
 				fmt.Printf("FILE:finSC\n")
-				l.Println(Nom, "Fin de Section critique")
+				stderr.Println(vert, Nom, "Fin de Section critique", raz)
 
 				mutex.Unlock()
 				pendingTransactions = []Transaction{}
@@ -94,7 +102,7 @@ func sendMain() {
 				// On ne demande qu'une seule fois l'accès à la section critique
 				fmt.Printf("FILE:demandeSC\n")
 				isSCAsked = true
-				l.Println(Nom, "Demande Section critique")
+				stderr.Println(orange, Nom, "Demande Section critique", raz)
 
 				time.Sleep(time.Duration(2) * time.Second)
 			} else {
@@ -107,7 +115,7 @@ func sendMain() {
 
 				mutex.Unlock()
 
-				l.Println(Nom, "Nouvelle transaction envoyée")
+				stderr.Println(Nom, "Nouvelle transaction envoyée")
 
 				time.Sleep(time.Duration(5) * time.Second)
 			}
@@ -122,7 +130,7 @@ func sendMain() {
 
 			mutex.Unlock()
 
-			l.Println(Nom, "Nouvelle transaction envoyée")
+			stderr.Println(Nom, "Nouvelle transaction envoyée")
 			time.Sleep(time.Duration(10) * time.Second)
 		}
 	}
@@ -148,21 +156,21 @@ func receive() {
 		if rcvmsg[:2] == "K:" {
 			otherKey := ReceivePublicKey(rcvmsg[2:])
 			adressOfSite = append(adressOfSite, otherKey)
-			l.Println(Nom, "Clé des autres : ", adressOfSite)
+			stderr.Println(Nom, "J'ai reçu", len(adressOfSite), "clé(s)")
 
 		} else if rcvmsg[:2] == "T:" {
-			l.Println(Nom, "Nouvelle transaction reçu")
+			stderr.Println(Nom, "Nouvelle transaction reçu")
 			newTransaction := ReceiveTransaction(rcvmsg[2:])
 			if newTransaction.Verify(&blockChain) {
 				pendingTransactions = append(pendingTransactions, newTransaction)
-				l.Println(Nom, "Nouvelle transaction ajoutée")
+				stderr.Println(Nom, "Nouvelle transaction ajoutée")
 			} else {
-				l.Println(Nom, "Nouvelle transaction refusée")
+				stderr.Println(Nom, "Nouvelle transaction refusée")
 			}
 
 		} else if rcvmsg[:2] == "B:" {
 			// vérifier la blockChain
-			l.Println(Nom, "Nouveau bloc reçu")
+			stderr.Println(Nom, "Nouveau bloc reçu")
 			newBlock := ReceiveBlock(rcvmsg[2:])
 			if len(blockChain.Chain) == 0 {
 				//Bloc d'origine, il n'y a pas de vérification
@@ -171,14 +179,14 @@ func receive() {
 			} else {
 				if newBlock.VerifyBlock(blockChain) {
 					blockChain.AddBlock(newBlock)
-					l.Println(Nom, "Nouveau bloc ajouté")
+					stderr.Println(Nom, "Nouveau bloc ajouté")
 					//l.Println(Nom, blockChain)
 				} else {
-					l.Println(Nom, "Nouveau bloc rejeté")
+					stderr.Println(Nom, "Nouveau bloc rejeté")
 				}
 			}
 		} else if rcvmsg == "debutSC" {
-			l.Println(Nom, "Autorisation de write")
+			stderr.Println(vert, Nom, "J'entre dans la section critique", raz)
 			allowSC = true
 		}
 		mutex.Unlock()
@@ -204,7 +212,7 @@ func main() {
 	}
 
 	time.Sleep(time.Duration(1) * time.Second)
-	l.Println(Nom, "Je commence mon initialisation")
+	stderr.Println(Nom, "Je commence mon initialisation")
 
 	go sendInitialisation()
 	go receive()
