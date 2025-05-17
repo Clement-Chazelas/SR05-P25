20c2dfb1532eb5a01e5367a0623aff33de08ff21
gjulie <58985394+gjulie@users.noreply.github.com>
2025-05-17 11:22:19 +0200

    Update snapshot.go

--- snapshot.go
+++ snapshot.go
@@ -1,207 +1,151 @@
 package main
 
 import (
-	"encoding/gob"
 	"fmt"
-	"log"
 	"os"
 	"sort"
-	"sync"
+	"strconv"
+	"strings"
 	"time"
 )
 
-var l = log.New(os.Stderr, "", log.LstdFlags)
+type messageSnapType string
+
+const (
+	prepost messageSnapType = "pre"
+	state messageSnapType = "sta"
+)
 
 // HORLOGE VECTORIELLE
+var vectorClock []int = make([]int, NbSite)
 
-type VectorClock map[int]int
-
-func InitiateVectorClock(nbSites int) VectorClock {
-	vc := make(VectorClock)
-	for i := 0; i < nbSites; i++ {
-		vc[i] = 0
-	}
-	return vc
-}
-
-func (vc VectorClock) Increment(siteID int) {
-	vc[siteID]++
-}
-
-func (vc VectorClock) Update(otherVC VectorClock) {
-	for id, time := range otherVC {
-		if _, ok := vc[id]; !ok || vc[id] < time {
-			vc[id] = time
-		}
-	}
-	vc.Increment(MyId)
-}
-
-func copyVectorClock(vc VectorClock) VectorClock {
-	newVC := make(VectorClock)
-	for id, time := range vc {
-		newVC[id] = time
-	}
-	return newVC
-}
-
-func getCurrentVectorClock() VectorClock {
-	vc := make(VectorClock)
-
-	for i := 0; i < NbSite; i++ {
-		vc[i] = 0 // On initialise les autres sites avec zéro
-	}
-
-	return vc
-}
+// VARIABLES
+var localSnapshot *Snapshot = nil
+var color string = "white"
+var initiator bool = false
 
 // SNAPSHOT
 type Snapshot struct {
-	Blockchain    Blockchain
-	UTXOSet       UTXOSet
-	VectorClock   VectorClock
+	LocalState 		string // A voir comment on récupère la blockchain
+	VectorClock   []int // Pour savoir quand a été prise la snapshot
 	Timestamp     time.Time
-	ChannelStates map[int][]string
+	ChannelStates [][]string // Messages dans canal pendant prepost
+	//PendingTransaction [][]Transaction
 }
 
-// Fonction pour enregistrer les snapshots
-// func (s Snapshot) Save(filename string) error {
-// 	// sauvegarde snapshot
-// }
-
-// VARIABLES
-const markerMessageType = "marker"
-
-var hasTakenLocalSnapshot = false
-var receivedMarkers = make(map[int]bool)
-var channelStates = make(map[int][]string)
-var snapshotState Snapshot
-var snapshotVectorClock VectorClock
-var snapshotMutex sync.Mutex
-
-// FONCTIONS POUR ALGO
-// Fonction pour initialiser l'instantané
-func InitiateDistributedSnapshot(b Blockchain) {
-	snapshotMutex.Lock()
-	defer snapshotMutex.Unlock()
-
-	fmt.Println(Nom, ": Initiation de l'instantané réparti")
-	hasTakenLocalSnapshot = true
-
-	snapshotState = TakeLocalSnapshot(b, b.GetLastBlock().UTXOs, getCurrentVectorClock())
-	snapshotVectorClock = snapshotState.VectorClock
-
-	// Envoi des marqueurs
-	snapshotState.ChannelStates = make(map[int][]string)
-	for i := 0; i < NbSite; i++ {
-		if i != MyId {
-			snapshotState.ChannelStates[i] = []string{}
-			sendSnapshotMessage(Sites[i], markerMessageType, nil)
-		}
-	}
-	fmt.Println(Nom, ": État local enregistré, marqueurs envoyés.")
+// Utilitaires
+func copyVectorClock(clock []int) []int {
+	copyClock := make([]int, len(clock))
+	copy(copyClock, clock)
+	return copyClock
 }
 
-// Fonction qui gère la réception d'un marqueur
-func ReceiveSnapshotMessage(senderName string, messageType string, data string) {
-	senderID := sort.SearchStrings(Sites, senderName)
-
-	snapshotMutex.Lock()
-	defer snapshotMutex.Unlock()
-
-	if messageType == markerMessageType {
-		fmt.Println(Nom, ": Marqueur reçu de", senderName)
-
-		// Si pas encore instantané local
-		if !hasTakenLocalSnapshot {
-			fmt.Println(Nom, ": Premier marqueur reçu. Enregistrement de l'état local.")
-			hasTakenLocalSnapshot = true
-			snapshotState = TakeLocalSnapshot(blockchain, blockchain.GetLastBlock().UTXOs, getCurrentVectorClock())
-			snapshotVectorClock = snapshotState.VectorClock
-
-			// Initialisation des états des canaux pour les sites
-			snapshotState.ChannelStates = make(map[int][]string)
-			for i := 0; i < NbSite; i++ {
-				if i != MyId {
-					snapshotState.ChannelStates[i] = []string{}
-					sendSnapshotMessage(Sites[i], markerMessageType, nil)
-				}
+func UpdateVectorClock(msg string) {
+	rcvClockStr := findval(msg, MsgHorloge)
+	if rcvClockStr != "" {
+		parts := strings.Split(rcvClockStr, ",")
+		for i, val := range parts {
+			intVal, _ := strconv.Atoi(val)
+			if intVal > vectorClock[i] {
+				vectorClock[i] = intVal
 			}
 		}
+	}
+	vectorClock[MyId]++
+}
 
-		receivedMarkers[senderID] = true // Le site a envoyé un marqueur
-		snapshotState.ChannelStates[senderID] = channelStates[senderID] // Sauvegarde les états des canaux du site
-		channelStates[senderID] = []string{} // Réinitialise les canaux du site
-
-	} else {
-		if hasTakenLocalSnapshot && !receivedMarkers[senderID] {
-			fmt.Println(Nom, ": Message application '", data, "' reçu de", senderName, "pendant l'instantané.")
-			channelStates[senderID] = append(channelStates[senderID], data)
+func mergeVectorClocks(vc1, vc2 []int) []int {
+	merged := make([]int, len(vc1))
+	for i := range vc1 {
+		if vc1[i] > vc2[i] {
+			merged[i] = vc1[i]
+		} else {
+			merged[i] = vc2[i]
 		}
 	}
-
-	// Vérification si tous les sites ont leur marqueur
-	allMarkersReceived := true
-	for i := 0; i < NbSite; i++ {
-		if i != MyId && !receivedMarkers[i] {
-			allMarkersReceived = false
-			break
-		}
-	}
-
-	// Si l'instantané local a été pris et que tous les marqueurs ont été reçus
-	if hasTakenLocalSnapshot && allMarkersReceived {
-		fmt.Println(Nom, ": Instantané global terminé.")
-		saveGlobalSnapshot()
-		resetSnapshotState()
-	}
+	return merged
 }
 
-func resetSnapshotState() {
-	hasTakenLocalSnapshot = false
-	receivedMarkers = make(map[int]bool)
-	channelStates = make(map[int][]string)
-	snapshotState = Snapshot{}
-	snapshotVectorClock = nil
-}
+// Fonction de début, initialisation de la snapshot par un site
+func InitSnapshot() {
+	color = "red"
+	initiator = true
 
-// Fonction qui enregistre l'état global
-func saveGlobalSnapshot() {
-	filename := fmt.Sprintf("snapshot-%s-%s.gob", Nom, time.Now().Format("20060102150405"))
-	snapshotState.VectorClock = snapshotVectorClock
-	// Enregistrer le snapshot
-	// err := snapshotState.Save(filename)
-	// if err != nil {
-	// 	fmt.Println(Nom, ": Erreur lors de la sauvegarde de l'instantané global:", err)
-	// }
-}
-
-// Fonction d'envoi de message
-func sendSnapshotMessage(receiverName string, messageType string, data interface{}) {
-	msg := MsgFormat(MsgSender, Nom) +
-		MsgFormat(MsgCategory, snapshot) +
-		MsgFormat(MsgType, messageType)
-	if data != nil {
-		msg += MsgFormat(MsgData, fmt.Sprintf("%v", data))
-	}
-	fmt.Println(receiverName, msg)
-}
-
-// Fonction qui est appelée dans le contrôleur
-func HandleSnapshotMessage(msg string) {
-	sender := findval(msg, MsgSender)
-	msgType := findval(msg, MsgType)
-	data := findval(msg, MsgData)
-	ReceiveSnapshotMessage(sender, msgType, data)
-}
-
-// Fonction de copie de l'état local
-func TakeLocalSnapshot(b Blockchain, u UTXOSet, vc VectorClock) Snapshot {
-	return Snapshot{
-		Blockchain:    b, // ou copie
-		UTXOSet:       u,
-		VectorClock:   copyVectorClock(vc),
+	// Sauvegarde locale
+	snapshotData = &Snapshot{
+		LocalState:    "état blockchain courant initial", // à adapter
+		VectorClock:   copyVectorClock(vectorClock),
+		ChannelStates: make([][]string, NbSite),
 		Timestamp:     time.Now(),
-		ChannelStates: make(map[int][]string),
+	}
+
+	sendSnapshotMessage(prepost)
+}
+
+// Fonction d'envoi des messages
+func sendSnapshotMessage(msgType messageSnapType) {
+	vectorClock[MyId]++
+
+	strs := make([]string, len(vectorClock))
+	for i, v := range vectorClock {
+		strs[i] = strconv.Itoa(v)
+	}
+	vcString := strings.Join(strs, ",")
+
+	formattedMsg := MsgFormat(MsgSender, Nom) +
+		MsgFormat(MsgCategory, snapshot) +
+		MsgFormat(MsgType, string(msgType)) +
+		MsgFormat(MsgHorloge, vcString) +
+		MsgFormat(MsgColor, color)
+		
+	fmt.Println(formattedMsg)
+}
+
+
+// Fonction pour la réception d'un message applicatif
+func ReceiveAppMessage(msg string){
+	UpdateVectorClock(msg)
+
+	c := findval(msg, MsgColor)
+	if c == "red" && color == "white" {
+		color = "red"
+
+		localSnapshot = &Snapshot{
+			LocalState:    "état blockchain courant au déclenchement", // à adapter
+			VectorClock:   copyVectorClock(vectorClock),
+			ChannelStates: make([][]string, NbSite),
+			Timestamp:     time.Now(),
+		}
+		sendSnapshotMessage(state)
+	}
+
+	if c == "white" && color == "red" {
+		sendSnapshotMessage(prepost)
+	}
+}
+
+func ReceivePrepostMessage(msg string) {
+	UpdateVectorClock(msg)
+
+	rcvData := findval(msg, MsgData)
+
+	if initiator {
+		sender := findval(msg, MsgSender)
+		sdrId := sort.SearchStrings(Sites, sender)
+		if localSnapshot != nil {
+			localSnapshot.ChannelStates[sdrId] = append(localSnapshot.ChannelStates[sdrId], rcvData)
+		}
+	} else {
+		sendSnapshotMessage(prepost)
+	}
+}
+
+func ReceiveStateMessage(msg string) {
+	UpdateVectorClock(msg)
+
+	if initiator {
+		localSnapshot.VectorClock = mergeVectorClocks(localSnapshot.VectorClock, vectorClock)
+	} else {
+		sendSnapshotMessage(state)
 	}
 }
