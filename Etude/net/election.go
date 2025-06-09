c7bedc4e8439dae791d90d437a5d0fdb2a27b797
Kinshoop <82682248+Kinshoop@users.noreply.github.com>
2025-06-09 19:11:22 +0200

    Update election.go

--- Etude/net/election.go
+++ Etude/net/election.go
@@ -1,35 +1,22 @@
 package main
 
 import (
-	"fmt"
 	"math"
 	"slices"
 	"strconv"
-	"strings"
-)
-
-const (
-	rouge       = "rouge"
-	bleu        = "bleu"
-	nombreSites = "nbsites"
 )
 
 var (
-	elu               = math.MaxInt
-	parent            = 0
-	enfants           = make([]int, 0)
-	nbVoisinsAttendus = NbVoisins
-	nbDescendants     = 0 // Utilisé pour compter le nombre total de sites
-	win               = false
+	parentTmp = 0
 )
 
 func DemarrerElection() {
-	if parent == 0 {
+	if parentTmp == 0 {
 		stderr.Println(Nom, "Je démarre mon elec")
 		elu = MyId
-		parent = MyId
+		parentTmp = MyId
 		nbVoisinsAttendus = NbVoisins
-		envoyerA(bleu, strconv.Itoa(elu), ListVoisins)
+		envoyerA(election, bleu, strconv.Itoa(elu), ListVoisins)
 	}
 }
 
@@ -39,44 +26,36 @@ func RecevoirMessageBleu(msg string) {
 	if k < elu {
 		stderr.Println(Nom, "Je change de vague pour", k)
 		elu = k
-		parent = senderId
-		enfants = make([]int, 0)
+		parentTmp = senderId
 		nbVoisinsAttendus = NbVoisins - 1
-		nbDescendants = 0
 		if nbVoisinsAttendus > 0 {
-			envoyerAuxVoisinsSauf(bleu, strconv.Itoa(elu), senderId)
+			envoyerAuxVoisinsSauf(election, bleu, strconv.Itoa(elu), senderId)
 		} else {
-			// j'envoie à mon parent mon nombre de descendants
-			envoyerAuParent(rouge, strconv.Itoa(elu), nbDescendants)
+			// j'envoie rouge à mon parent
+			destinataire := []int{parentTmp}
+			envoyerA(election, rouge, strconv.Itoa(elu), destinataire)
 		}
 	} else if elu == k {
 		destinataire := []int{senderId}
-		envoyerA(rouge, strconv.Itoa(elu), destinataire)
+		envoyerA(election, rouge, strconv.Itoa(elu), destinataire)
 	}
 }
 
 func RecevoirMessageRouge(msg string) {
 	k, _ := strconv.Atoi(findval(msg, MsgData))
-	senderId, _ := strconv.Atoi(findval(msg, MsgSender))
 	if elu == k {
 
 		nbVoisinsAttendus--
-		if findval(msg, "enfant") != "" {
-			// Le message a été envoyé par mon enfant
-			enfants = append(enfants, senderId)
-			nbRecu, _ := strconv.Atoi(findval(msg, "enfant"))
-			nbDescendants += nbRecu + 1
-		}
 		stderr.Println(Nom, "Je reduit mon nombre de voisins", nbVoisinsAttendus)
 		if nbVoisinsAttendus == 0 {
 			if elu == MyId {
-
 				win = true
-				NbSites = nbDescendants + 1
 				stderr.Println(Nom, "J'ai gagné", NbSites)
 			} else {
-				// j'envoie à mon parent mon nombre de descendants
-				envoyerAuParent(rouge, strconv.Itoa(elu), nbDescendants)
+				// j'envoie rouge à mon parent
+				destinataire := []int{parentTmp}
+				envoyerA(election, rouge, strconv.Itoa(elu), destinataire)
+				resetElection()
 			}
 		}
 	}
@@ -93,55 +72,16 @@ func recevoirMessageElection(msg string) {
 	switch findval(msg, MsgType) {
 	case bleu:
 		RecevoirMessageBleu(msg)
+		break
 	case rouge:
 		RecevoirMessageRouge(msg)
-	case nombreSites:
-		NbSites, _ = strconv.Atoi(findval(msg, MsgData))
-		// Je transmets à mes enfants le nombre de sites
-		envoyerA(nombreSites, strconv.Itoa(NbSites), enfants)
+		break
 	}
 }
 
-func envoyerAuxVoisinsSauf(couleur string, data string, saufVoisin int) {
-	if !slices.Contains(ListVoisins, saufVoisin) {
-		stderr.Println(red, Nom, "Voisin non présent dans la liste", saufVoisin, ListVoisins, raz)
-		return
-	}
-	vIndex := slices.Index(ListVoisins, saufVoisin)
-	tmpSlice := make([]int, len(ListVoisins))
-	copy(tmpSlice, ListVoisins)
-	msg := MsgFormat(MsgSender, strconv.Itoa(MyId)) + MsgFormat(MsgCategory, electionInit) + MsgFormat(MsgType, couleur) +
-		MsgFormat(MsgData, data) +
-		MsgFormat(MsgDestination, intTabToStr(append(tmpSlice[:vIndex], tmpSlice[vIndex+1:]...)))
-	fmt.Println(msg)
-}
-
-func envoyerAuParent(msgType string, data string, nbDescendants int) {
-	msg := MsgFormat(MsgSender, strconv.Itoa(MyId)) + MsgFormat(MsgCategory, electionInit) + MsgFormat(MsgType, msgType) +
-		MsgFormat(MsgData, data) + MsgFormat(MsgDestination, strconv.Itoa(parent)) +
-		MsgFormat("enfant", strconv.Itoa(nbDescendants))
-	fmt.Println(msg)
-}
-
-func envoyerA(msgType string, data string, destinaires []int) {
-	msg := MsgFormat(MsgSender, strconv.Itoa(MyId)) + MsgFormat(MsgCategory, electionInit) + MsgFormat(MsgType, msgType) +
-		MsgFormat(MsgData, data) + MsgFormat(MsgDestination, intTabToStr(destinaires))
-	fmt.Println(msg)
-}
-
-func intTabToStr(voisins []int) string {
-	strs := make([]string, len(voisins))
-	for i, v := range voisins {
-		strs[i] = strconv.Itoa(v)
-	}
-	return strings.Join(strs, ",")
-}
-
-func strToIntTab(voisinsStr string) []int {
-	parts := strings.Split(voisinsStr, ",")
-	voisins := make([]int, len(parts))
-	for i, val := range parts {
-		voisins[i], _ = strconv.Atoi(val)
-	}
-	return voisins
+func resetElection() {
+	elu = math.MaxInt
+	nbVoisinsAttendus = NbVoisins
+	parentTmp = 0
+	win = false
 }
