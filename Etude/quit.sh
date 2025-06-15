a6b6aeba8bff8ebb72b4b4cc8c567d220d906fc1
Kinshoop <82682248+Kinshoop@users.noreply.github.com>
2025-06-15 18:35:05 +0200

    Add files via upload

--- Etude/quit.sh
+++ Etude/quit.sh
@@ -10,7 +10,7 @@ fi
 
 # Le premier argument est le numéro du site parent
 parent_site=$1
-# Supprime le premier argument pour ne garder que les enfants
+
 shift
 # Le reste des arguments sont les numéros des sites enfants
 children_sites=("$@")
@@ -18,34 +18,30 @@ children_sites=("$@")
 # Chemin vers le fichier contenant les informations du site parent
 parent_file="/tmp/${parent_site}.txt"
 
-# Vérifier si le fichier du parent existe
-if [ ! -f "$parent_file" ]; then
-    echo "Erreur: Fichier de configuration $parent_file non trouvé."
-    exit 1
-fi
-
-# Lire la première ligne (chaîne des FIFOs) et la deuxième ligne (PID) du fichier du parent
+# Lit les informations du fichier parent : Nom de la fifo, argument du tee et PID du tee.
 parent_fifo_name=$(head -n 1 "$parent_file")
 current_fifo_line=$(head -n 2 "$parent_file" | tail -n 1)
 pid_parent=$(tail -n 1 "$parent_file")
 
 
-# Construire la nouvelle chaîne de FIFOs en ajoutant les FIFOs des enfants
+# Construire la nouvelle chaîne de FIFOs pour le tee en ajoutant les FIFOs des enfants
 new_parent_fifo_line="$current_fifo_line"
 for child in "${children_sites[@]}"; do
     
     child_file="/tmp/${child}.txt"
-    
+    # Récupération des informations de l'enfant
     child_fifo_name=$(head -n 1 "$child_file")
     current_fifo_line_child=$(head -n 2 "$child_file" | tail -n 1)
     pid_child=$(tail -n 1 "$child_file")
+    
+    # Ajout de la fifo du parent au tee de l'enfant
     new_fifo_line_child="$current_fifo_line_child /tmp/in_N$parent_fifo_name"
     
-    #echo "Nouvelles FIFOs à utiliser pour l'enfant $child: $new_fifo_line_child"
-    
+    # Kill du processus cat et tee de l'enfant
     kill -KILL $(pgrep -f /tmp/out_N$child_fifo_name) 
     kill -KILL $pid_child
-
+    
+    # Nouveau cat/tee pour l'enfant
     cat "/tmp/out_N${child_fifo_name}" | tee $new_fifo_line_child > /dev/null &
     new_pid_child=$! # Capture le nouveau PID du processus 'tee' de l'enfant
     
@@ -54,18 +50,18 @@ for child in "${children_sites[@]}"; do
     echo "$child_fifo_name" > "$child_file"
     echo "$new_fifo_line_child" >> "$child_file"
     echo "$new_pid_child" >> "$child_file"
-
+    
+    # Ajout des arguments du tee du parent en ajoutant l'enfant
     new_parent_fifo_line="$new_parent_fifo_line /tmp/in_N$child_fifo_name"
 done
 
-#echo "Nouvelles FIFOs à utiliser pour le parent: $new_parent_fifo_line"
-
+# Kill du cat et tee du parent
 kill -KILL $(pgrep -f /tmp/out_N$parent_fifo_name) 
 kill -KILL $pid_parent
 
-# Relancer la commande 'cat | tee' avec les nouvelles FIFOs
+# Nouveau cat/tee pour le parent
 cat "/tmp/out_N${parent_fifo_name}" | tee $new_parent_fifo_line > /dev/null &
-new_pid=$! # Capture le nouveau PID du processus 'tee'
+new_pid=$! 
 
 # Mettre à jour le fichier de configuration du parent avec les nouvelles informations
 echo "$parent_fifo_name" > "$parent_file"
